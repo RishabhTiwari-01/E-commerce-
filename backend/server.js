5ff75c3d9e45a0dc9f2a9db49a9d23221152eac4
Rishabh Tiwari <rt451495@gmail.com>
2025-12-16 16:47:43 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config"
-import connectDB from "./config/mongodb.js";
-import connectCloudinary from "./config/cloudinary.js";
-import userRouter from "./routes/userRoute.js";
-import productRouter from "./routes/productRoute.js";
-import cartRouter from "./routes/cartRoute.js";
-import orderRouter from "./routes/orderRoute.js";
+// import express from "express";
+// import cors from "cors";
+// import "dotenv/config"
+// import connectDB from "./config/mongodb.js";
+// import connectCloudinary from "./config/cloudinary.js";
+// import userRouter from "./routes/userRoute.js";
+// import productRouter from "./routes/productRoute.js";
+// import cartRouter from "./routes/cartRoute.js";
+// import orderRouter from "./routes/orderRoute.js";
+
+
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv/config');
+const connectDB = require('./config/mongodb.js');
+const connectCloudinary = require('./config/cloudinary.js');
+const userRouter = require('./routes/userRoute.js');
+const productRouter = require('./routes/productRoute.js');
+const cartRouter = require('./routes/cartRoute.js');
+const orderRouter = require('./routes/orderRoute.js');
+
+
 
 // App config
 
